372cb02d2583ce35bee829f4b593f708c0d4b888
Lance Hepler <nlhepler@gmail.com>
2011-11-22 15:54:26 -0800

    update the log format to include an MRMR token

--- lib/mrmr/_logging.py
+++ lib/mrmr/_logging.py
@@ -9,6 +9,6 @@ MRMR_LOGGER = 'M4zhcs3U6vNLPLF8nNZkX75G'
 def _setup_log():
     import logging
     h = logging.StreamHandler()
-    f = logging.Formatter('%(levelname)s %(asctime)s %(process)d %(funcName)s: %(message)s')
+    f = logging.Formatter('%(levelname)s %(asctime)s %(process)d MRMR %(funcName)s: %(message)s')
     h.setFormatter(f)
     logging.getLogger(MRMR_LOGGER).addHandler(h)
